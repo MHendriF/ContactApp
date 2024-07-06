ca72a4082c2454b83095c63b266df2e2da4aaeec
MHendriF <hendrifebriansyah28@gmail.com>
2024-07-06 20:30:38 +0700

    add constants

---
+++ app/src/main/java/com/mhendrif/contactapp/utils/Constants.kt
@@ -0,0 +1,7 @@
+package com.mhendrif.contactapp.utils
+
+object Constants {
+    const val DATABASE_NAME = "contact_local.db"
+    const val DB_VERSION = 1
+    const val CONTACT_TABLE_NAME = "contacts"
+}
